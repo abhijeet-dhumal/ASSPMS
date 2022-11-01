9c8a980ec0c6688f7458fb7e6876a8932644f681
abhijeet-dhumal <abhijeetdhumal652@gmail.com>
2022-11-01 14:37:13 +0530

    minor change

--- app/settings.py
+++ app/settings.py
@@ -107,6 +107,9 @@ DATABASES = {
     }
 }
 
+# import dj_database_url
+# DATABASES = {'default': dj_database_url.parse('postgres://user:password@host:post/database')}
+
 AUTH_USER_MODEL = 'account.User'
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -142,7 +145,7 @@ USE_I18N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
