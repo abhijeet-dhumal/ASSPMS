5f99ff544ff9bc2d38ce22479c2757d8a29aa03b
abhijeet-dhumal <abhijeetdhumal652@gmail.com>
2023-04-17 11:28:45 +0530

    vercel related bug fixes

--- app/settings.py
+++ app/settings.py
@@ -160,7 +160,7 @@ USE_I18N = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media/')
--- build_files.sh
+++ build_files.sh
@@ -7,6 +7,6 @@ python3.9 manage.py collectstatic --noinput --clear
 echo "============> STATIC FILES COLLECTED <==============="
 
 echo "============> MAKE-MIGRATIONS <==============="
-python3.9 manage.py makemigartions --noinput
+python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 echo "============> MAKE-MIGRATIONS-END <==============="
