3a1e9f28f03695bd370e19179eb1c873bb2dcd5f
Abhijeet Dhumal <84722973+abhijeet-dhumal@users.noreply.github.com>
2024-03-31 10:25:27 +0530

    Update run.sh to add instructions to create default users

--- run.sh
+++ run.sh
@@ -3,6 +3,11 @@
 python manage.py makemigrations
 python manage.py migrate --no-input
 
+#create default admin user
+echo "from django.contrib.auth import get_user_model; CustomUser = get_user_model();  CustomUser.objects.create_superuser('admin@gmail.com', 'admin_password')" | python manage.py shell
+#create default simple user
+echo "from django.contrib.auth import get_user_model; CustomUser = get_user_model();  CustomUser.objects.create_user('simple_user@gmail.com', 'simple_user_password')" | python manage.py shell
+
 gunicorn app.wsgi:application --bind 0.0.0.0:8000 &
 
 unlink /etc/nginx/sites-enabled/default
